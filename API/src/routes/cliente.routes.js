19c74b5d7101a852a776a2747bec7c669ad6bbb0
João Pedro <joaopedromalagi@hotmail.com>
2024-08-26 16:12:04 -0300

    feat: create routes

---
+++ API/src/routes/cliente.routes.js
@@ -0,0 +1,6 @@
+const router = require('express-promise-router');
+
+router.post('/cliente/novo');
+router.get('/cliente/listar');
+
+module.exports = router;
---
+++ API/src/routes/hotel.routes.js
@@ -0,0 +1,6 @@
+const router = require('express-promise-router');
+
+router.post('/hotel/novo');
+router.get('/hotel/listar');
+
+module.exports = router;
