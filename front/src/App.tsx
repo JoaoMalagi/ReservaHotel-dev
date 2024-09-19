b5b607a5cd77a69586e8172acccc0a4bd85cf25c
João Pedro <joaopedromalagi@hotmail.com>
2024-09-19 10:40:57 -0300

    Adicionando frontend

---
+++ front/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Usuario from './components/Usuario'
+
+function App() {
+  
+
+  return (
+    <><nav>
+      <Navbar />
+    </nav>
+      <div>
+        <Usuario />
+      </div>
+    </>
+  )
+}
+
+export default App
